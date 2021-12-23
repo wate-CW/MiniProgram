f16872ce118ac0a37f362380cad266313e5ffc10
wate-CW <1175213472@qq.com>
2021-12-23 22:13:00 +0800

    初始化项目

---
+++ pages/abnomal/abnomal.js
@@ -0,0 +1,84 @@
+// pages/abnomal/abnomal.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  /**
+   * 用户点击上传异常按钮跳转到上传异常资料Page
+   */
+  upAbnomalPage:function(){
+    wx.navigateTo({
+      url: '../abnomal/up-abnomal-page/up-abnomal',
+      events: console.log('跳转'),
+      success: (result) => {
+        console.log(result.errMsg)
+      },
+      fail: (res) => {
+        console.log(res.errMsg)
+      },
+      complete: (res) => {},
+    })
+
+
+  }
+})
